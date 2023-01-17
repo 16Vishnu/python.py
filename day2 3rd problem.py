78c399b6b160003271589d371487aaf4bf24ef0a
16Vishnu <122291766+16Vishnu@users.noreply.github.com>
2023-01-17 17:32:27 +0530

    Delete day2 3rd problem.py

--- day2 3rd problem.py
+++
@@ -1,6 +0,0 @@
-countofwords = len("Hello This is python programming".split())
-print("count of words in the given sentence:",countofwords)
-
-print(len("hello this is python programming".split()))
-
-print(len(input("Enter Input:").split()))
